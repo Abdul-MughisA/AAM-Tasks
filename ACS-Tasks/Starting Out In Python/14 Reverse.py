8bd606f386b7c149bcc5ff0e8a5e56906ae30341
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-09-18 19:53:02 +0100

    Reverse work.

--- ACS-Tasks/Starting Out In Python/14 Reverse.py
+++ ACS-Tasks/Starting Out In Python/14 Reverse.py
@@ -1,10 +1,16 @@
 #This task simply reverses the input.
 
-sentence = input("Enter an input: ")
-for i in range(0, len(sentence)):
-    front = sentence[i]
-    back = sentence[len(sentence)-i]
-    sentence[i] = back
-    sentence[len(sentence)-i] = front
+sentence = input("Enter an input to be reversed: ")
+newSentence = ""
+
+#Counts a for loop from the length of the sentence down to -1 (because for loops are not inclusive) with step -1.
+#With each step down, the newSentence variable has the letter appended.
+#So that as the for loop continues, the reverse letter continues to be appended to the newSentence.
+#And at the end, then, newSentence will hold the whole reversed sentence.
+for i in range(len(sentence)-1, -1, -1):
+    newSentence = newSentence + sentence[i]
 #end for
-print(sentence)
+
+#Outputs the reverse in a user-friendly format.
+print("Reverse:")
+print(newSentence)
