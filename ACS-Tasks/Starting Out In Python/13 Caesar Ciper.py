49d4b1162d1e4f1e60da2bad9bc9a3ed9ad7c87f
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-09-19 22:00:39 +0100

    Caesar Cypher finished.

--- ACS-Tasks/Starting Out In Python/13 Caesar Ciper.py
+++ ACS-Tasks/Starting Out In Python/13 Caesar Ciper.py
@@ -1,6 +1,9 @@
 #This program encodes text into a Caesar Cipher by taking in the sentence and the offset.
 
-print("Do not enter any punctuation: ")
+temp = ""
+finalSentence = ""
+
+print("Do not enter any punctuation or numbers: ")
 originalSentence = input("Enter the sentence you would like to encode: ")
 print("Present your offset in the form of an integer. To offset a sentence by 1 letter, enter 1.")
 offset = int(input("Enter the desired offset: "))
@@ -15,6 +18,16 @@ modSentence = modSentence.replace(" ", "")
 
 #Uses a for loop with range as long as the length of the sentence to add the offset to each character to shift it.
 #If adding the offset makes the ASCII go past the letter z, then just subtract 26 from the ASCII code.
-for change in range(0, len(modSentence)):
-    print(modSentence[0])
+for i in range(0, len(modSentence)):
+    temp = ord(modSentence[i]) + offset
+    
+    #Checks whether adding offset goes papst z and then adjusts accordingly.
+    if temp > 122:
+        temp = temp - 26
+    #end if
+    
+    temp = chr(temp)
+    finalSentence = finalSentence + temp
 #end for
+
+print(finalSentence)
