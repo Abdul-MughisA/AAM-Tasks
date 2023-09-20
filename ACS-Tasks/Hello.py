fe7338536c4b56bff22828f47a6b65232588f3c9
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-09-20 11:24:45 +0100

    Update Hello.py

--- ACS-Tasks/Hello.py
+++ ACS-Tasks/Hello.py
@@ -1,2 +1,18 @@
 # This program prints "Hello, World!".
 print("Hello, world!")
+
+year = int(input("Enter a year: "))
+LeapYear = False
+if year % 4 == 0:
+	LeapYear = True
+elif year % 100 == 0:
+	LeapYear = False
+
+if year % 400 == 0:
+	LeapYear = True
+
+if LeapYear == True:
+	print(year, "is a leap year.")
+
+if LeapYear == False:
+	print(year, "is not a leap year.")
