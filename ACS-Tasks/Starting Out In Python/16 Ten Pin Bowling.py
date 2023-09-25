e402d8437881be8f7308a1c3280fa23f872b3b29
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-09-25 01:14:05 +0100

    Update 16 Ten Pin Bowling.py

--- ACS-Tasks/Starting Out In Python/16 Ten Pin Bowling.py
+++ ACS-Tasks/Starting Out In Python/16 Ten Pin Bowling.py
@@ -17,31 +17,61 @@
 # The final score is all the frames added together (including strike and spare).
 
 print("This is a Ten Pin Bowling score calculator.")
-hits = ["",""]
-finalScore = ""
-counterStrike = 0
-counterSpare = 0
-frame = ""
+score = {}
+final = 0
+spare1 = 0
+spare2 = 0
 
-for i in range(1, 11):
+# Sets all values to 0 to prevent out-of-range error.
+for a in range(0,12):
+    score[a,0] = 0
 
-    # Input score for first hit.
-    hits[i,1] = input("Enter the score for hit", i + ": ")
+    for b in range(0,2):
+        score[a,b] = 0
+    #end for
+#end for
 
-    #Checks for strike.
-    if hits[i,1] == 10:
-        counterStrike =+ 3
+# Asks user for inputs (working correctly).
+for i in range(0,10):
+    score[i,0] = int(input("Enter the score on the first bowl: "))
+
+    if score[i,0] < 10:
+        score[i,1] = int(input("Enter the score on the second bowl: "))
+        if score[i,0] + score[i,1] == 10:
+            print("Spare!")
+        #end if
     else:
-        #Only asks for this if there is no strike.
-        hits[i,2] = input("Enter the score for 2nd hit", i + ": ")
+        print("Strike!")
+    #end if
+#end for
 
-    if hits[i,1] + hits[i,2] > 10:
-        print("An error has occured.")
-    elif hits[i,1] + hits[i,2] == 10:
-        counterSpare =+ 1
+if score[9,0] == 10:
+    spare1 = int(input("Enter the score on the spare bowl: "))
+    spare2 = int(input("Enter the score on the spare bowl: "))
+elif score[9,0] + score[9,1] == 10:
+    spare1 = int(input("Enter the score on the spare bowl: "))
+#end if
 
-    if counterStrike > 0:
-        finalScore =+ frame
-        counter =- 1
-    
-# CALCULATE THE STRIKE AND SPARE INFORMATION IN A SEPARATE LOOP IN THE END BY QUERYING IF THE SCORES ADD UP TO 10.
+for j in range(0,10):
+    # Adds the strike.
+    if score[j,0] == 10:
+        plus1 = j + 1
+        plus2 = j + 2
+        final = final + score[plus1,0] + score[plus2,0]
+    #end if
+
+    #Adds the spare.
+    if (score[j,0] + score[j,1]) == 10 and score[j,0] != 10:
+        plus1 = j + 1
+        final = final + score[plus1,0]
+
+    #end if
+
+    final = final + score[j,0]
+    final = final + score[j,1]
+
+#end for
+
+final = final + spare1 + spare2
+
+print(final)
