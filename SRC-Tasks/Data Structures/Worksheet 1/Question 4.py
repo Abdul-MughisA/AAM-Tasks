9480494333913d4bea04cc1030b28be1440c2d2b
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-10-08 16:21:11 +0100

    Worksheet tasks

---
+++ SRC-Tasks/Data Structures/Worksheet 1/Question 4.py
@@ -0,0 +1,7 @@
+# Same as question 3 but with more outlets.
+
+total = [0 for _ in range(3)]
+for quarter in range(3):
+    for outlet in range(49):
+        total[quarter] = total[quarter]  + outletSales[quarter,outlet]
+    print(total[quarter])
---
+++ SRC-Tasks/Data Structures/Worksheet 1/Question 5.py
@@ -0,0 +1,3 @@
+# Program doesn’t mention needing this game to repeat, so coded as if this game only loops once.
+
+# No 2D arrays in Python?
