ee823b7594a05c96665e386f742ea8b4d0b399e1
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-10-10 14:47:43 +0100

    Carroll Python

--- SRC-Tasks/Data Structures/Worksheet 1/Question 4.py
+++ SRC-Tasks/Data Structures/Worksheet 1/Question 4.py
@@ -1,5 +1,7 @@
 # Same as question 3 but with more outlets.
 
+
+
 total = [0 for _ in range(3)]
 for quarter in range(3):
     for outlet in range(49):
--- SRC-Tasks/Data Structures/Worksheet 1/Question 5.py
+++ SRC-Tasks/Data Structures/Worksheet 1/Question 5.py
@@ -1,3 +1,5 @@
 # Program doesn’t mention needing this game to repeat, so coded as if this game only loops once.
 
-# No 2D arrays in Python?
+grid = [["x" for _ in range(4)] for __ in range(6)]
+grid[0][0] = "O"
+print(grid)
