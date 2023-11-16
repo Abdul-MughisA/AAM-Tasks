c6ff011eeed6421943d5ebf8173f0a27f41a0197
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-11-16 12:00:48 +0000

    Linked lists work

--- Personal/Testing1.py
+++ Personal/Testing1.py
@@ -1,22 +1,8 @@
-loop1 = 0
+import time
 
-list1 = [2,5,15,36,47,56,59,78,156,244,268]
-list2 = [18,39,42,43,66,69,100]
+test = 0
 
-list1.sort()
-list2.sort()
-for loop2 in range(len(list2)-1, -1, -1):
-    moveNum = list2[loop2]
-    loop = True
-    while loop:
-        loop1 += 1
-        refNum = list1[loop1]
-        if moveNum < refNum:
-            list1.insert(loop1, list2.pop(loop2))
-            loop1 = 0
-            loop = False
-        #end if
-    #end while
-#end for
-
-print(list1)
+while True:
+    print(test)
+    time.sleep(5)
+#end while
--- SRC-Tasks/Data Structures/Linked Lists/List 1.py
+++ SRC-Tasks/Data Structures/Linked Lists/List 1.py
@@ -5,18 +5,18 @@ class Node:
     #end def
 
     def __repr__(self) -> str:
-        return "Data:", self.data, "Pointer:", self.pointer
+        return "Data: " + self.data + ", Pointer: " + str(self.pointer)
     #end def
 #end class
 
 linkedListData = []
-linkedListData.append(Node("Empty", 1))
-linkedListData.append(Node("Empty", 2))
-linkedListData.append(Node("Empty", 3))
+linkedListData.append(Node("Lucas", 2))
+linkedListData.append(Node("A'yaan", 0))
+linkedListData.append(Node("Suren", -1))
 linkedListData.append(Node("Empty", 4))
 linkedListData.append(Node("Empty", -1))
 
-startPointer = -1
-nextFreePointer = 0
+startPointer = 1
+nextFreePointer = 3
 
 print(linkedListData)
