1a900821b685b9f6272afcd69bcef55281d01020
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2024-01-12 15:11:26 +0000

    More sorting

--- SRC-Tasks/Algorithms/Sort Algorithms/Bubble Sort/Bubble Sort.py
+++ SRC-Tasks/Algorithms/Sort Algorithms/Bubble Sort/Bubble Sort.py
@@ -1,6 +1,13 @@
-def swap(a, b, array):
+array = [7, 4, 6, 8, 1, 5]
 
-    
+def swap(a, b, array):
+    temp = array[b]
+    array[b] = array[a]
+    array[a] = temp
 #end def
 
-array = [7, 4, 6, 8, 1, 5]
+print(array)
+swap(0, 1, array)
+
+# MAKE THE BUBBLE SORT STOP WHEN NO CHANGES ARE MADE ON A PASS #
+# USUALLY THE BUBBLE SORT STOPS AFTER THE LENGTH OF THE ARRAY  #
